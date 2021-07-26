80fd769e6d094faeee8a48d54efed7794f95ef97
proelbtn <contact@proelbtn.com>
2021-07-26 09:42:39 +0900

    add EDITOR environment variable

---
+++ scripts/setup-darwin.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+silent() {
+  $@ >/dev/null 2>/dev/null
+  return $?
+}
+
+expand() {
+  local dir="$(cd "$(dirname "$1")"; pwd -P)"
+  local base="$(basename "$1")"
+
+  if [[ "$1" = "/" ]]; then
+    echo "/"
+  elif [[ "$dir" = "/" ]]; then
+    echo "$dir$base"
+  else
+    echo "$dir/$base"
+  fi
+}
+
+symlink() {
+  local src=$(expand $1)
+  local dst=$(expand $2)
+
+  printf "[link] ${dst} -> ${src}\n" 
+  ln -sf "${src}" "${dst}"
+}
+
+cd $(dirname $0)/..
+
+
+# zsh
+symlink ./zsh/zshenv ~/.zshenv
+symlink ./zsh/zshrc ~/.zshrc
+
+
+# brew
+silent which hub || brew install hub
+silent which nvim || brew install neovim
+silent which tig || brew install tig
+silent which wireshark || brew install --cask wireshark
