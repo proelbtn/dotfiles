97d85375015a36b8cb18ed7aaaebc50420be9176
Ryoga Saito <contact@proelbtn.com>
2026-02-05 00:37:40 +0900

    Add empty zshrc and starship config

---
+++ scripts/setup-darwin.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd "$(dirname "$0")/.."
+
+link() {
+    ln -sf "$(realpath $1)" "$2"
+}
+
+command_exists() {
+    which "$1" >/dev/null 2>&1
+}
+
+command_exists starship || brew install starship
+
+link ./starship/starship.toml ~/.config/starship.toml
+
+link ./zsh/.zshrc ~/.zshrc
